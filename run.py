ba5435cb38c89b5cfe1da3a17ea865c103872b88
Matex600 <mat.leks@gmail.com>
2021-10-05 21:02:01 +0000

    add initial functions and update start screen, import random

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
-# Your code goes here.
+import random
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Welcome to Minesweeper")
-print("Start by entering username!")
+print("--------- Welcome to Minesweeper ---------")
+print(" --------- --------- --------- ---------")
+print("------ Start by entering username! ------")
 
 username = input()
 
-print("Welcome, " + username + ", have fun!")
+print("----- Welcome, " + username + ", good luck have fun! -----")
+
+# def initialize_game_board():
+"""
+"""
+# def inject_bombs():
+"""
+"""
+# def player_input():
+"""
+"""
+# def game_over():
+"""
+"""
+# def main():
