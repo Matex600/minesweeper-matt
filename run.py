b2115dd015a6e0888c67d6f86cb1c780c8900b83
Matex600 <mat.leks@gmail.com>
2021-10-06 20:03:51 +0000

    add improved grid game board with space for bombs and clear cells

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import os
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 print("--------- Welcome to Minesweeper ---------")
@@ -8,25 +9,68 @@ print("------ Start by entering username! ------")
 username = input()
 
 print("----- Welcome, " + username + ", good luck & have fun! -----\n")
+mine_values = 8
+j = 8
 
-def initialize_game_board(i, j):
+def initialize_game_board():
     """
     Create game board using columns and rows
     """
     print("    Game started ... \n")
-    board = [["." for row in range(i)] for column in range(j)]
-    for row in board:
-        print(" ".join(str(cell) for cell in row))
-        print("")
-# def inject_bombs():
-"""
-"""
+    global mine_values
+    global j
+    
+
+    print()
+    
+    cell_block = "   "
+    for i in range(j):
+        cell_block = cell_block + "     " + str(i + 1)
+    print(cell_block)
+
+    for r in range(j):
+        cell_block = "     "
+        if r == 0:
+            for col in range(j):
+                cell_block = cell_block + "______"  
+            print(cell_block)
+
+        cell_block = "     "
+        for col in range(j):
+            cell_block = cell_block + "|     "
+        print(cell_block + "|")
+
+        cell_block = "  " + str(r + 1) + "  "
+        for col in range(j):
+            cell_block = cell_block + "|  " + str(mine_values[r][col]) + "  "
+        print(cell_block + "|")
+
+        cell_block = "     "
+        for col in range(j):
+            cell_block = cell_block + "|_____"
+        print(cell_block + "|")  
+
+    print()
+
+# def inject_bombs(bombs):
+    # """
+    # inject randomised bombs to game board
+    # """
+    # for
+
 # def player_input():
 """
 """
+
+
 # def game_over():
 """
 """
+
+
 def main():
-    initialize_game_board(12, 6)
-main()    
+    initialize_game_board()
+    
+
+main()
+
