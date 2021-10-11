32695d572e7f4698ed37a3129baf0e68895da7b9
Matex600 <mat.leks@gmail.com>
2021-10-11 18:09:58 +0000

    remove global variables and define variables at start of code, replace j with grid_size to improve readability

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
 import random
 import os
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-global j
-global mine_values
-global mine_amount
-global numbers
+
+# Size of game grid (6x6)
+grid_size = 6
+
+#  Visible values of the grid
+mine_values = [[' ' for y in range(grid_size)] for x in range(grid_size)]
+
+# Actual (hidden) values of the grid
+numbers = [[0 for y in range(grid_size)] for x in range(grid_size)]
+
+# Tracking umber of mines already set starts at 0
+num_mines_present = 0
+
+max_mine_num = 6
+
 
 # Welcome message and username prompt
 print("--------- Welcome to Minesweeper ---------")
@@ -29,38 +39,36 @@ def initialize_game_board():
     for columns and rows through the use of for loops
     """
     print("    Game starting ... \n")
-    j = 6
-    mine_values = [[' ' for y in range(j)] for x in range(j)]
+
     print()
-    
     # Layout of Minesweeper game area
     cell_block = "   "
-    for i in range(j):
+    for i in range(grid_size):
         cell_block = cell_block + "     " + str(i + 1)
     print(cell_block)
 
     # For loop creates squares in the grid using | and _
-    for r in range(j):
+    for r in range(grid_size):
         cell_block = "     "
         if r == 0:
-            for col in range(j):
-                cell_block = cell_block + "______"  
+            for col in range(grid_size):
+                cell_block = cell_block + "______"
             print(cell_block)
 
         cell_block = "     "
-        for col in range(j):
+        for col in range(grid_size):
             cell_block = cell_block + "|     "
         print(cell_block + "|")
 
         cell_block = "  " + str(r + 1) + "  "
-        for col in range(j):
+        for col in range(grid_size):
             cell_block = cell_block + "|  " + str(mine_values[r][col]) + "  "
         print(cell_block + "|")
 
         cell_block = "     "
-        for col in range(j):
+        for col in range(grid_size):
             cell_block = cell_block + "|_____"
-        print(cell_block + "|")  
+        print(cell_block + "|")
 
     print()
 
@@ -70,33 +78,22 @@ def inject_bombs():
     This function stores data and values from the mines
     and flags
     """
-    # Size of grid (6x6)
-    j = 6
-    # Number of mines present
-    mine_amount = 4
-
-    # Actual (hidden) values of the grid
-    numbers = [[0 for y in range(j)] for x in range(j)]
-    # Visible values of the grid
-    mine_values = [[' ' for y in range(j)] for x in range(j)]
 
     # Flagged positions
     flag = []
-
-    # Tracking umber of mines already set starts at 0
-    mine_count = 0
-    while mine_count < mine_amount:
+    num_mines_present = 0
+    while num_mines_present < max_mine_num:
 
         # Random number for grid positions
-        grid_positions = random.randint(0, j*j -1)
+        grid_positions = random.randint(0, grid_size*grid_size - 1)
 
         # Generate row and column from numbers in grid
-        r = grid_positions // j
-        col = grid_positions % j
+        r = grid_positions // grid_size
+        col = grid_positions % grid_size
 
         # Add a mine if there are none on the grid
         if numbers[r][col] != - 1:
-            mine_count = mine_count + 1
+            num_mines_present = num_mines_present + 1
             numbers[r][col] = - 1
 
 
@@ -106,15 +103,10 @@ def actual_board_values():
     the player and work in the background, checking the board for
     presence of mines using a for loop
     """
-    global j
-    global numbers
-
-    j = 6
-    numbers = [[0 for y in range(j)] for x in range(j)]
 
     # Loop that counts every cell in the grid
-    for r in range(j):
-        for col in range(j):
+    for r in range(grid_size):
+        for col in range(grid_size):
             
             # Skips check if a mine is present
             if numbers[r][col] == - 1:
@@ -125,15 +117,15 @@ def actual_board_values():
                 numbers[r][col] = numbers[r][col] + 1
 
             # Checks input below
-            if r < j-1 and numbers[r+1][col] == - 1:
+            if r < grid_size-1 and numbers[r+1][col] == - 1:
                 numbers[r][col] = numbers[r][col] + 1
 
             # Checks left input
             if col > 0 and numbers[r][col-1] == - 1:
-                numbers[r][c] = numbers[r][c] + 1
+                numbers[r][col] = numbers[r][col] + 1
 
             # Checks right input
-            if col < j - 1 and numbers[r][col+1] == - 1:
+            if col < grid_size - 1 and numbers[r][col+1] == - 1:
                 numbers[r][col] = numbers[r][col] + 1
 
             # Checks top left input
@@ -141,24 +133,22 @@ def actual_board_values():
                 numbers[r][col] = numbers[r][col] + 1
 
             # Checks top right input
-            if r > 0 and col < j - 1 and numbers[r-1][col+1] == - 1:
+            if r > 0 and col < grid_size - 1 and numbers[r-1][col+1] == - 1:
                 numbers[r][col] = numbers[r][col] + 1
 
             # Checks bottom left input
-            if r < j - 1 and col > 0 and numbers[r+1][col-1] == - 1:
+            if r < grid_size - 1 and col > 0 and numbers[r+1][col-1] == - 1:
                 numbers[r][col] = numbers[r][col] + 1
 
             # Checks bottom right input
-            if r < j - 1 and col < j - 1 and numbers[r + 1][col + 1] == - 1:
-                numbers[r][col] = numbers[r][col] + 1   
+            if r < grid_size - 1 and col < grid_size - 1 and numbers[r+1][col+1] == -1:
+                numbers[r][col] = numbers[r][col] + 1
                         
 
-
 # def player_input():
-
+ # if user_input in range 6
 
 # def game_over():
-
 
 def main():
     initialize_game_board()
